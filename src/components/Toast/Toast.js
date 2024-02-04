9ae2e9271af9122eddd28d9537d2a57db42367af
Lucy Macartney <macartney.lucy@gmail.com>
2024-02-04 13:28:24 -0500

    tweaks to solution

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -18,15 +18,15 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast({ message, variant, closeToast }) {
+function Toast({ variant, children, handleDismiss }) {
   const Icon = ICONS_BY_VARIANT[variant];
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
         <Icon size={24} />
       </div>
-      <p className={styles.content}>{message}</p>
-      <button onClick={closeToast} className={styles.closeButton}>
+      <p className={styles.content}>{children}</p>
+      <button onClick={handleDismiss} className={styles.closeButton}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -8,12 +8,12 @@ import styles from "./ToastPlayground.module.css";
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
-  const [isVisible, setIsVisible] = React.useState(false);
+  const [isRendered, setIsRendered] = React.useState(false);
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
 
-  const closeToast = () => {
-    setIsVisible(false);
+  const handleDismiss = () => {
+    setIsRendered(false);
   };
 
   return (
@@ -22,8 +22,10 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-      {isVisible && (
-        <Toast message={message} variant={variant} closeToast={closeToast} />
+      {isRendered && (
+        <Toast variant={variant} handleDismiss={handleDismiss}>
+          {message}
+        </Toast>
       )}
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -71,7 +73,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button onClick={() => setIsVisible(true)}>Pop Toast!</Button>
+            <Button onClick={() => setIsRendered(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
